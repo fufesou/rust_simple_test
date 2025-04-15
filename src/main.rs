9a647e24c8d4a94eb8320b065a69c27902841a1a
fufesou <linlong1266@gmail.com>
2025-04-15 20:43:15 +0800

    test
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use std::io::Read;
+use sysinfo::{Pid, System};
 
-use cpal::traits::{DeviceTrait, HostTrait};
+fn get_pids_of_process_with_args<S1: AsRef<str>, S2: AsRef<str>>(
+    name: S1,
+    args: &[S2],
+) -> Vec<Pid> {
+    let name = name.as_ref().to_lowercase();
+    let mut system = System::new_all();
+    system.refresh_processes();
+    system
+        .processes()
+        .iter()
+        .filter(|(_, process)| {
+            process.name().to_lowercase() == name
+                && process.cmd().len() == args.len() + 1
+                && args.iter().enumerate().all(|(i, arg)| {
+                    process.cmd()[i + 1].to_lowercase() == arg.as_ref().to_lowercase()
+                })
+        })
+        .map(|(&pid, _)| pid)
+        .collect()
+}
 
 fn main() {
-    let host = cpal::default_host();
-    let device = host.default_output_device().unwrap();
     println!(
-        "Default output device: {}",
-        device.name().unwrap_or("".to_owned())
+        "PIDs of processes with name 'rustdesk' and args ['--tray']: {:?}",
+        get_pids_of_process_with_args("rustdesk.exe", &["--tray"])
     );
-    println!("\nPress any key to exit...");
-    let _ = std::io::stdin().read(&mut [0u8]).unwrap();
 }
