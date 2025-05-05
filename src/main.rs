7fc5b399343388f3e7e067282f31123374ceb9e0
fufesou <linlong1266@gmail.com>
2025-05-05 20:51:46 +0800

    test
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use std::io::Read;
+use rdev::{simulate, EventType, Key, SimulateError};
+use std::{thread, time};
 
-use cpal::traits::{DeviceTrait, HostTrait};
+fn send(event_type: &EventType) {
+    let delay = time::Duration::from_millis(20);
+    match simulate(event_type) {
+        Ok(()) => (),
+        Err(SimulateError) => {
+            println!("We could not send {:?}", event_type);
+        }
+    }
+    // Let ths OS catchup (at least MacOS)
+    thread::sleep(delay);
+}
 
 fn main() {
-    let host = cpal::default_host();
-    let device = host.default_output_device().unwrap();
-    println!(
-        "Default output device: {}",
-        device.name().unwrap_or("".to_owned())
-    );
-    println!("\nPress any key to exit...");
-    let _ = std::io::stdin().read(&mut [0u8]).unwrap();
+    let k_shift = Key::RawKey(rdev::RawKey::WinVirtualKeycode(50));
+    let k_r_bracket = Key::RawKey(rdev::RawKey::WinVirtualKeycode(34));
+    send(&EventType::KeyPress(k_shift.clone()));
+    send(&EventType::KeyPress(k_r_bracket.clone()));
+    send(&EventType::KeyRelease(k_r_bracket));
+    send(&EventType::KeyRelease(k_shift));
 }
